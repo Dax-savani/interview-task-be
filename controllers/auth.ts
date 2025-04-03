061536131b8229a3bc04bf61f96d2cd3010bbe4f
Dax-savani <dakshsavani15@gmail.com>
2025-04-03 18:18:29 +0530

    expiration

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 
 const generateTokens = (userId: string) => {
-    const accessToken = jwt.sign({ userId }, process.env.JWT_SECRET!, { expiresIn: "15m" });
+    const accessToken = jwt.sign({ userId }, process.env.JWT_SECRET!, { expiresIn: "1d" });
     const refreshToken = jwt.sign({ userId }, process.env.JWT_REFRESH_SECRET!, { expiresIn: "7d" });
     return { accessToken, refreshToken };
 };
